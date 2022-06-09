247256ba3d706b620f596f7242e6b71ad4246ec5
crpz1 <8588315+crpz1@users.noreply.github.com>
2022-06-09 22:02:20 +1000

    remove essential
    there is literally no reason for it to be a dependency
    it isnt used anywhere

--- src/main/java/club/sk1er/mods/scrollabletooltips/ScrollableTooltips.java
+++ src/main/java/club/sk1er/mods/scrollabletooltips/ScrollableTooltips.java
@@ -1,6 +1,5 @@
 package club.sk1er.mods.scrollabletooltips;
 
-import gg.essential.api.EssentialAPI;
 import net.minecraftforge.common.ForgeVersion;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
@@ -10,17 +9,19 @@ public class ScrollableTooltips {
 
     // todo: migrate modid to scrollable_tooltips
     public static final String MOD_ID = "text_overflow_scroll";
-    public static final String MOD_VERSION = "1.4.0";
+    public static final String MOD_VERSION = "1.4.1";
     public static final String MOD_NAME = "Scrollable Tooltips";
 
     @Mod.EventHandler
     public void loadComplete(FMLLoadCompleteEvent event) {
         //noinspection ConstantConditions
+        /*
         if (!ForgeVersion.getVersion().contains("2318") && ForgeVersion.mcVersion.equalsIgnoreCase("1.8.9")) {
             EssentialAPI.getNotifications().push(
                     "Outdated Forge",
                     "Scrollable Tooltips will not work on anything below Forge build 2318.\nPlease consider updating Forge."
             );
         }
+        */
     }
 }
